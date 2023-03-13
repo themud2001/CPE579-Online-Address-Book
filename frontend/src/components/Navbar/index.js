b7793be592e51b68103d2e02fbbb0bb6b9988482
themud2001 <themud2001@hotmail.com>
2023-03-13 21:21:19 +0300

    A little bit of styling

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import { FaBookReader } from "react-icons/fa";
 
 const EnhancedNavbar = () => {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar className="sticky-top" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Online Address Book</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <FaBookReader className="me-3" />
+                    Online Address Book
+                </Navbar.Brand>
+
                 <Button variant="light">Log In</Button>
             </Container>
         </Navbar>
