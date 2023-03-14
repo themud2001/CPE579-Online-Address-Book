b3c52efeeaa9325993fc47193a94879c5a4b4d1e
themud2001 <themud2001@hotmail.com>
2023-03-14 18:13:13 +0300

    Added validation for the sign-in form

--- frontend/src/components/SignIn/index.js
+++ frontend/src/components/SignIn/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const LogIn = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleFormOnSubmit = formData => {
+        console.log(formData);
+    };
+
     return (
         <Container className="d-flex justify-content-center mt-5">
             <Card className="shadow p-4">
@@ -13,15 +20,25 @@ const LogIn = () => {
 
                     <hr />
 
-                    <Form>
+                    <Form onSubmit={handleSubmit(handleFormOnSubmit)}>
                         <Form.Group className="mb-3" controlId="username">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Username" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Username"
+                                {...register("username", { required: "Username is required" })}
+                            />
+                            {errors.username && <Form.Text style={{ color: "red" }}>{errors.username.message}</Form.Text>}
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="password">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                {...register("password", { required: "Password is required" })}
+                            />
+                            {errors.password && <Form.Text style={{ color: "red" }}>{errors.password.message}</Form.Text>}
                         </Form.Group>
 
                         <Button variant="primary" type="submit">
