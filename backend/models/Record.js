dcc6e8a264d0f34c6b7b0c254d0008b708049a91
themud2001 <themud2001@hotmail.com>
2023-03-24 03:21:22 +0300

    Removed the random-words package and the dev lines of code

--- backend/models/Record.js
+++ backend/models/Record.js
@@ -1,9 +1,6 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
 
-// DEV ONLY
-// const randomWords = require("random-words");
-
 const Record = sequelize.define("records", {
     id: {
         type: DataTypes.INTEGER,
@@ -34,16 +31,5 @@ const Record = sequelize.define("records", {
 }, {
     timestamps: false
 });
-
-// DEV ONLY
-// for (let i = 0; i < 15; i++) {
-//     Record.create({
-//         name: randomWords(),
-//         address: randomWords(),
-//         phone: randomWords(),
-//         workField: randomWords(),
-//         coordinates: randomWords()
-//     });
-// }
 
 module.exports = Record;
