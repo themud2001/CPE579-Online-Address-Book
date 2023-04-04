e9c07e29ef1ecc9de62d19fd6bdb8031a010dafb
themud2001 <themud2001@hotmail.com>
2023-04-04 21:28:29 +0300

    Added a logo for the website

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -3,12 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import { FaBookReader } from "react-icons/fa";
 import { BiLogIn } from "react-icons/bi";
 import Button  from "react-bootstrap/Button";
 
 import { useFetchAccountDetailsQuery, signOut } from "../../store";
 import "./style.scss";
+import logo from "./logo_white.png";
 
 const EnhancedNavbar = () => {
     const dispatch = useDispatch();
@@ -19,7 +19,8 @@ const EnhancedNavbar = () => {
         <Navbar className="sticky-top custom-gradient shadow-sm" bg="primary" variant="dark">
             <Container>
                 <Link to="/" className="navbar-brand">
-                    <FaBookReader className="me-3" />
+                    <img src={logo} height="40" width="40" />
+                    
                     Online Address Book
                 </Link>
 
